150a4eff15b0673eaf1337e61053e209578fc99d
HopCoder <mrfredericks@gmail.com>
2013-11-14 08:18:35 -0500

    Fixed make script.  Made it nicer. Redirected unecessary output to /dev/null

--- make_library.sh
+++ make_library.sh
@@ -1,11 +1,17 @@
-tar -zxvf rtmidi-2.0.1.tar.gz
-cd rtmidi-2.0.1
-./configure
+echo extracting libraries
+tar -zxvf rtmidi-2.0.1.tar.gz > /dev/null
+cd rtmidi-2.0.1 > /dev/null
+echo configuring libraries
+./configure > /dev/null
+echo building libraries
+make > /dev/null
+cd tests > /dev/null
+echo building included .o files
+make > /dev/null
+cd ../.. > /dev/null
+echo building project
 make
-cd tests
-make
-cd ../..
-make
-rm -rf rtmidi-2.0.1
-make
+echo removing static libraries
+rm -rf rtmidi-2.0.1 > /dev/null
+echo build successfull
 
