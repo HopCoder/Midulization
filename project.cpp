e8647a0e62598fa7957531697720a1733b882aa9
Oranga <tangdtd@gmail.com>
2013-12-01 23:45:59 -0500

    Opacity fuck it

--- project.cpp
+++ project.cpp
@@ -28,7 +28,7 @@ static GLsizei width, height;
 static float warp = 0.2; //The amount of warpage in a given visualization
 static float up = 0.0; 
 static int isAnimate = 0;
-static int animateInterval = 50; //Time interval between frames.
+static int animateInterval = 10; //Time interval between frames.
 static float pointSize = 3.0;
 
 read_midi inputMidi;
@@ -64,45 +64,25 @@ float get_height_scalar(float x){
 }
 
 void add_note(unsigned char x, unsigned char a){
-    amp.push_back((float) a);
+    amp.push_back(32.0);
     B.push_back(PI * 2 * FREQ(x+1));
     //phase_shift.push_back((((int)x)*PI/6.0));
     phase_shift.push_back(0.0);
 }
   
 void animate(int value){
-  
-  
-
-  /*if (isAnimate){
-    xvector.push_back(0.0);
-    yvector.push_back(0.0);
-    for(int j = 0; j < xvector.size() - 1; j++){
-      xvector[j] = xvector[j] + warp;
-      if (yvector[j] > 0.0){
-	yvector[j] -= 0.5;
-      }
-      if (yvector[j] < 0.0)
-	yvector[j] = 0.0;
-
-      if (up >= 0.0){
-	yvector[j] += up;
-	up -= up/yvector.size();
-      }
-      
-      }
-
-    if (sides > 25)
-    sides = 4;
-    else
-    sides++;
-    
-    }*/
 
   keyvals = inputMidi.get_array();
   on_keys = inputMidi.get_on_keys();
   //if(on_keys.size()>0) std::cout << "test on keys " << (int)on_keys[0][0] << ' ' << (int)on_keys[0][1] << '\t';
   if(B.size() > 0) std::cout << amp[0] << '\t' << B[0]/(2*PI) << std::endl;
+
+  amp.clear();
+  phase_shift.clear();
+  B.clear();
+  for(unsigned int i = 0; i < on_keys.size(); i++){
+    add_note(on_keys[i][0], on_keys[i][1]);
+  }
 
   glutTimerFunc(animateInterval, animate, 1);
   glutPostRedisplay();
@@ -110,46 +90,98 @@ void animate(int value){
 
 void drawMe(void){
   
-  glClear(GL_COLOR_BUFFER_BIT);
-  glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
-  //glLoadIdentity();
-  //glTranslatef(0.0, 0.0, -25.0);
-  glColor3f(0.0, 1.0, 0.0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glEnable (GL_BLEND);
+  glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+  glLoadIdentity();
+  glTranslatef(0.0, 0.0, -1.001);
+  //glRotatef(PI, 0.0, 1.0, 0.0); 
+  //glColor3f(1.0, 1.0, 1.0);
   
   if (isAnimate) 
     for (int i = 24; i < keyvals.size(); i++){
       
       glutSolidTorus(3.0, 15.0, 5, 5); 
+      
     }
     
   else 
     {
-      amp.clear();
-      phase_shift.clear();
-      B.clear();
-      for(unsigned int i = 0; i < on_keys.size(); i++){
-          add_note(on_keys[i][0], on_keys[i][1]);
-      }
-
-      glBegin(GL_QUADS);
-      glColor3f(1.0, 0.0, 0.0);
       
-      while(frames.size() >= 10) frames.pop_front();
+      glColor3f(1.0, 0.0, 0.0);
+      glBegin(GL_QUADS);
+      for (float i = 0; i < width; i+=1){
+	glVertex3f(( i + 1 ), 
+		   height/2.0 + height/2.0 *get_height_scalar(i)/127.0, 0.0);
+	glVertex3f(( i + 1 ), 
+		   height/2.0, 0.0);
+	glVertex3f( i, height/2.0, 0.0);
+	glVertex3f( i , height/2.0 + height/2.0 * get_height_scalar(i)/127.0, 0.0);
+      }
+      glEnd();
+      /*
+      
+      glColor3f(0.0, 1.0, 0.0);
+      glBegin(GL_QUADS);
+      for (unsigned int i = 0; i < keyvals.size(); i++){
+	//glColor3f(0.0, 1.0 * (float)i /(float)keyvals.size(), 0.0);	    
+	glVertex3f((i + 1) * width / keyvals.size(), 
+		   height/2 + height/2 * (get_height_scalar(i))/127.0, 0.2);
+	glVertex3f((i + 1) * width / keyvals.size(), height/2, 0.2);
+	glVertex3f((i) * width / keyvals.size(), height/2, 0.2);
+	glVertex3f((i) * width / keyvals.size(), 
+		   height/2 + height/2 * (get_height_scalar(i))/127.0, 0.2);
+      }
+      glEnd();
+      glColor3f(0.0, 0.0, 1.0);
+      glBegin(GL_QUADS);
+      for (unsigned int i = 0; i < keyvals.size(); i++){
+	//glColor3f(0.0, 1.0 * (float)i /(float)keyvals.size(), 0.0);	    
+	glVertex3f((i) * width / (keyvals.size()), height/2, 0.2);
+	glVertex3f((i) * width / (keyvals.size()), 
+		   height/2 + height/2 * (get_height_scalar(i))/127.0, 0.2);	
+	glVertex3f((i) * width / (keyvals.size()), 
+		   height/2 + height/2 * (get_height_scalar(i))/127.0, 0.0);
+	glVertex3f((i) * width / (keyvals.size()), height/2, 0.0);
+	
+      }
+      glEnd();
+      glColor3f(1.0, 1.0, 1.0);
+      glBegin(GL_QUADS);
+      for (unsigned int i = 0; i < keyvals.size(); i++){
+	//glColor3f(0.0, 1.0 * (float)i /(float)keyvals.size(), 0.0);	    
+	glVertex3f((i + 1) * width / keyvals.size(), 
+		   height/2 + height/2 * (get_height_scalar(i))/127.0, 0.0);
+	glVertex3f((i + 1) * width / keyvals.size(), height/2, 0.0);
+	glVertex3f((i + 1) * width / keyvals.size(), height/2, 0.2);
+	glVertex3f((i + 1) * width / keyvals.size(), 
+		   height/2 + height/2 * (get_height_scalar(i))/127.0, 0.2);
+      }
+	  glEnd();
+      
+    }
+  */
+      glBegin(GL_QUADS);
+      glColor4f(1.0, 0.0, 0.0, 1.0);
+      
+      while(frames.size() >= 100) frames.pop_front();
 
       std::vector<float> last_frame;
       for(float i = 0; i < width; i+=width/500){
-        last_frame.push_back(height/2 + height/2 * (get_height_scalar(i)/127.0));
+        last_frame.push_back(height/2.0 + height/2.0 * get_height_scalar(i)/127.0);
       }
       frames.push_back(last_frame);
       
       std::list<std::vector<float> >::iterator it = frames.begin();
       for(unsigned int i = 0; i < frames.size(); i++){
-        glColor3f(0.1 * (i), 0.0, 0.1*(10 - i));
+        glColor4f(0.01 * (i), 0.0, 0.01*(100 - i),  2.0/(frames.size() - i));
+	float w = (frames.size() - i) * -0.1;
         for(unsigned int j = 0; j < it->size(); j++){
-	        glVertex3f((j*(width/500) + 1), (*it)[j], 0.0);
-	        glVertex3f((j*(width/500) + 1), height/2, 0.0);
-	        glVertex3f((j*(width/500)), height/2, 0.0);
-	        glVertex3f((j*(width/500)), (*it)[j], 0.0);	  
+	  glVertex3f((j*(width/500) + 1), (*it)[j], w);
+	  glVertex3f((j*(width/500) + 1), height/2.0, w );
+	  glVertex3f((j*(width/500)), height/2.0, w);
+	  glVertex3f((j*(width/500)), (*it)[j], w);	  
         }
         if(it == frames.end()) break;
         it++;
@@ -164,7 +196,8 @@ void drawMe(void){
 	
       }*/
       glEnd();
-
+      //gluLookat(width/2, height/2, 0.0, 		width/2, height/2, 0.0,);
+		
     }
   
   glutSwapBuffers();
@@ -212,8 +245,10 @@ void resize(int w, int h)
    glMatrixMode(GL_PROJECTION);
    glLoadIdentity();
 
-   //glFrustum(-5.0, 5.0, -5.0, 5.0, 5.0, 100.0);
-   glOrtho(0.0, (float)w, 0.0, (float)h, -1.0, 1.0);
+   //glFrustum(-(float)w /2.0, (float)w /2.0, 
+   //	     -(float)h /2.0, (float)h, 1.0, 50.0);
+   glFrustum(0.0, (float)w, 0.0, (float)h, 1.0, 50.0);
+   //glOrtho(0.0, (float)w, 0.0, (float)h, -1.0, 1.0);
    
    width = w;
    height = h;
@@ -228,7 +263,7 @@ int main (int argc, char** argv)
   glutInitDisplayMode(GLUT_SINGLE | GLUT_DOUBLE);
   glutInitWindowSize(500, 500);
   glutInitWindowPosition(100,100);
-  glutCreateWindow("GroovMe");
+  glutCreateWindow("Midulization");
   setup();
   glutDisplayFunc(drawMe);
   glutReshapeFunc(resize);
